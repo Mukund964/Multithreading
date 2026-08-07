07df572c060eec97f50295d5759945b9bc6db92d
mukund <16mukund2002@gmail.com>
2026-08-07 09:39:18 +0530

    Thread Safe Using Atomic Integer

--- src/Assesment_1/sharedResource_Counter.java
+++ src/Assesment_1/sharedResource_Counter.java
@@ -1,29 +1,30 @@
 // shared resource here is our counter, this is op that we are trying to perform
 package Assesment_1;
 
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class sharedResource_Counter {
-     int counter = 0;
-     ReentrantLock lock = new ReentrantLock();
+
+     AtomicInteger atomicCounter = new AtomicInteger(0);
 
     //using sync block critical section uses monitor lock on calling object but make it thread safe
     public void incrementCounter(){
-                lock.lock();
+
             try {
                 System.out.println(Thread.currentThread().getName() + " Entered Critical Section");
 
                 for (int i = 0; i < 10000; i++) {
-                    counter++;
+                    atomicCounter.incrementAndGet();
                 }
 
                 System.out.println("Critical Section End");
             } finally {
-                lock.unlock();
+                System.out.println("finished Critical Section");
             }
         }
 
     public int getCounter(){
-        return counter;
+        return atomicCounter.get();
     }
 }
