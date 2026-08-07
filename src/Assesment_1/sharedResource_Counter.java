a8de682e550010d99ce39232b8374f9e9ffd0b51
mukund <16mukund2002@gmail.com>
2026-08-07 09:19:32 +0530

    Fix of 2 obj accessing Same static resource using static lock. Reentrant lock

--- src/Assesment_1/sharedResource_Counter.java
+++ src/Assesment_1/sharedResource_Counter.java
@@ -1,19 +1,26 @@
 // shared resource here is our counter, this is op that we are trying to perform
 package Assesment_1;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class sharedResource_Counter {
     static int counter = 0;
+    static ReentrantLock lock = new ReentrantLock();
 
     //using sync block critical section uses monitor lock on calling object but make it thread safe
-    synchronized public void incrementCounter(){
-        System.out.println(Thread.currentThread().getName() + " Entered Critical Section");
+    public void incrementCounter(){
+        lock.lock();
+        try{
+            System.out.println(Thread.currentThread().getName() + " Entered Critical Section");
 
-        for(int i=0;i<10000;i++){
-            counter++;
+            for(int i=0;i<10000;i++){
+                counter++;
+            }
+
+            System.out.println("Critical Section End");
+        }finally {
+            lock.unlock();
         }
-
-        System.out.println("Critical Section End");
-
     }
 
     public int getCounter(){
